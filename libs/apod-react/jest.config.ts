d6f1a69ff3087c65a7450f50c9ece77397b2603b
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-06-05 15:13:00 -0700

    Add apod-react component

---
+++ libs/apod-react/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'apod-react',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/apod-react',
+};
