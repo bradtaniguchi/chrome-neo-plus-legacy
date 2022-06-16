534a86b71b03c0b8a33501d84c7841c6d31cec92
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-06-15 21:08:38 -0700

    fix typedoc compile issues

--- apps/chrome-extension/typedoc.js
+++ apps/chrome-extension/typedoc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entryPoints: ['./src/main.ts'],
+  entryPoints: ['./src/main.tsx'],
   tsconfig: './tsconfig.json',
   out: '../../dist/docs/chrome-extension',
   readme: './README.md',
--- apps/internal-client/typedoc.js
+++ apps/internal-client/typedoc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entryPoints: ['./src/main.ts'],
+  entryPoints: ['./src/main.tsx'],
   tsconfig: './tsconfig.json',
   out: '../../dist/docs/internal-client',
   readme: './README.md',
