ca4238cb4825f3cda8a50f1c81b1a730bc9ed0a3
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-06-15 20:56:42 -0700

    chore: add typedoc support for remaining projects
    Close #13

---
+++ apps/chrome-extension/typedoc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entryPoints: ['./src/main.ts'],
+  tsconfig: './tsconfig.json',
+  out: '../../dist/docs/chrome-extension',
+  readme: './README.md',
+};
---
+++ apps/internal-client/typedoc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entryPoints: ['./src/main.ts'],
+  tsconfig: './tsconfig.json',
+  out: '../../dist/docs/internal-client',
+  readme: './README.md',
+};
